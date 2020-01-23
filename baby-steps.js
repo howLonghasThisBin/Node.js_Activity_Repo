5c35ee2348d61aae551a7db803c64916a8a63cc4
howLonghasThisBin <mlonghas_180000001821@uic.edu.ph>
2020-01-23 08:26:09 +0800

    added BABY STEPS activity solution to repo via git.

--- baby-steps.js
+++ baby-steps.js
@@ -1,6 +1,6 @@
 var sum = 0;
 
-for (var i = 2; i < process.argv.length; i++) {
+for (var i = 2; i < process.argv.length; i++) { // process.argv == an array
     sum += Number(process.argv[i]);
 }
 
